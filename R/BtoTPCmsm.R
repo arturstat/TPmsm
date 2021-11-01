26461fad29a3a31b0f0fdc9c3c17e97dde058021
Artur Araujo <artur.stat@gmail.com>
2021-11-01 03:34:44 +0000

    Tidy code

--- R/BtoTPCmsm.R
+++ R/BtoTPCmsm.R
@@ -1,3 +1,10 @@
-BtoTPCmsm <- function(lst, UT, UX, s, t, x, statenames, nboot, conflevel, methodboot) {
-	return( .Call(Rf_BtoTPCmsm, lst, UT, UX, s, t, x, statenames, as.integer(nboot), conflevel, methodboot, PACKAGE="TPmsm") )
-}
+BtoTPCmsm <- function(
+  lst, UT, UX, s, t, x, statenames, nboot, conflevel, methodboot
+) {
+  return(
+    .Call(
+      Rf_BtoTPCmsm, lst, UT, UX, s, t, x, statenames,
+      as.integer(nboot), conflevel, methodboot, PACKAGE="TPmsm"
+    )
+  );
+} # BtoTPCmsm
