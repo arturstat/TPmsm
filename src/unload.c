f74cbfa9f3676fc56c3189769f242a69f6270b3d
Artur Araujo <artur.stat@gmail.com>
2023-01-13 19:00:58 +0000

    Update unload.c

--- src/unload.c
+++ src/unload.c
@@ -7,7 +7,7 @@
 #include "RngArray.h"
 #include "rthreads.h"
 
-void attribute_visible R_unload_TPmsm() {
+void attribute_visible R_unload_TPmsm(void) {
 	RngArray_DeleteStream(&global_num_procs, RngArray);
 	free(RngArray);
 	return;
